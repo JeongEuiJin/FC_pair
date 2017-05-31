4360785f42d1cf6b5123e0be13dcffea79297867
JeongEuiJin <hehar1020@gmail.com>
2017-05-31 21:13:34 +0900

    form , add 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from blog.forms import PostCreateForm
 from blog.models import Post
 
 User = get_user_model()
+
 
 def post_list(request):
     posts = Post.objects.order_by('-created_date')
@@ -13,10 +15,40 @@ def post_list(request):
     }
     return render(request, 'blog/post_list.html', context)
 
-def post_detail(request, pk):
 
+def post_detail(request, pk):
     context = {
         'posts': Post.objects.get(pk=pk),
 
     }
     return render(request, 'blog/post_detail.html', context)
+
+
+def post_add(request):
+    form = PostCreateForm()
+    #form 태그 전달받을때 기본적으로 GET으로 받는게 맞나요?
+    #
+    if request.method == 'GET':
+        print("Get run...")
+        context = {
+            'form': form
+        }
+        return render(request, 'blog/post_add.html', context)
+    elif request.method == 'POST':
+        print("POST run...")
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            text = form.cleaned_data['text']
+            user = User.objects.first()
+            post = Post.objects.create(
+                title=title,
+                text=text,
+                author=user
+            )
+            return redirect('post_detail', pk=post.pk)
+        else:
+            context = {
+                'form': form
+            }
+            return render(request, 'post_add.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$',views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/add/$', views.post_add, name='post_add')
 ]
